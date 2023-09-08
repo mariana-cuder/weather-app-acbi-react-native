43d72e418bb16fec1d475e95f10b3698ced91c5b
mariana-cuder <144402236+mariana-cuder@users.noreply.github.com>
2023-09-07 22:43:00 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { 
+          StyleSheet,
+
+        } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+import HomeScreen from './components/HomeScreen';
+import SettingsScreen from './components/SettingsScreen';
+
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  
+
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+            
+            if (route.name === 'Home') {
+              iconName = 'home';
+            }
+            if (route.name === 'Settings'){
+              iconName = 'settings';
+            }
+             return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      
+      >
+        <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
+        <Tab.Screen name="Settings" component={SettingsScreen} options={{ title: 'Settings' }}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+    
+    
+    
+  );
+}
+console.reportErrorsAsExceptions = false;
+
+
+const styles = StyleSheet.create({
+  
+});
